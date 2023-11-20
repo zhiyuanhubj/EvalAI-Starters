15a1c44d1eddd0a3dbe1b29fb8d684283ed90125
Zhiyuan Hu <zhiyuan.hu.bj@gmail.com>
2023-11-20 11:57:32 +0800

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -42,16 +42,9 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     output = {}
     if phase_codename == "dev":
         print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            }
-        ]
+        with open(user_submission_file,'r') as f:
+            data = json.load(f)
+        output["result"] = data
         # To display the results in the result file
         output["submission_result"] = output["result"][0]["train_split"]
         print("Completed evaluation for Dev Phase")
